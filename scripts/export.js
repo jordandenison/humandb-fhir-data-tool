c5264148290612591809a0e39dbafd63b3b6dcf3
Jordan Denison <jordan@denisonweb.com>
2019-02-28 19:19:01 -0600

    add file targets to export and import scripts

--- scripts/export.js
+++ scripts/export.js
@@ -1,5 +1,6 @@
 const { getFhirResourceJSON } = require('../src/lib/fhir')
 
 const datatype = process.argv[2] || 'stu2'
+const fileTarget = process.argv[3] || './export.json'
 
-getFhirResourceJSON(datatype, './export.json')
+getFhirResourceJSON(datatype, fileTarget)
--- scripts/import.js
+++ scripts/import.js
@@ -1,5 +1,6 @@
 const { writeFhirResourceJSON } = require('../src/lib/fhir')
 
 const datatype = process.argv[2] || 'stu2'
+const fileTarget = process.argv[3] || './export.json'
 
-writeFhirResourceJSON(datatype)
+writeFhirResourceJSON(datatype, fileTarget)
